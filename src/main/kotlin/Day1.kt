1c8e9a076201af7f60cee589f74462762e76823d
martwebs <mwebster1973@gmail.com>
2023-12-01 07:11:44 +0000

    First Attempt - Yuck

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -1,9 +1,35 @@
 class Day1 {
-    fun day1(): Int {
-        return 10
+
+    companion object {
+        private var digits = listOf("zero","one","two","three","four","five","six","seven","eight","nine")
+        fun getValues(line: String): Int {
+
+            val values = mutableListOf<Int>()
+            var lettersSoFar = ""
+            for (char in line) {
+                if (char.isDigit()) {
+                    values.add(char.toString().toInt())
+                    lettersSoFar = ""
+                }
+                else {
+                    lettersSoFar += char
+                    val digitsFound = digits.find { lettersSoFar.contains(it) }
+                    if (digitsFound != null) {
+                        values.add(digits.indexOf(digitsFound))
+                        lettersSoFar = char.toString()
+                    }
+                }
+            }
+
+            return "${values.first()}${values.last()}".toInt()
+        }
+
+        fun sumValues(line: List<String>): Int {
+            return line.sumOf { getValues(it) }
+        }
     }
 
-    fun addTwoNumbers(a: Int, b: Int): Int {
-        return a + b
-    }
+
+
+
 }
--- src/test/kotlin/Day1Test.kt
+++ src/test/kotlin/Day1Test.kt
@@ -5,15 +5,45 @@ import org.junit.jupiter.api.Assertions.*
 class Day1Test {
 
     @Test
-    fun day1() {
+    fun part1And2() {
 
-        val data = Util().readData("fred.txt")
-        assertEquals(2,data.size)
+        val data = Util().readData("day1.txt")
+        //assertEquals(56042,Day1.sumValues(data)) part1And2
+        assertEquals(55358,Day1.sumValues(data)) //part2
     }
 
     @Test
-    fun addTwoNumbers() {
+    fun samplePart1GetValues() {
+
+        val sampleData = listOf("1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet")
+
+        assertEquals(12, Day1.getValues(sampleData[0]))
+        assertEquals(38, Day1.getValues(sampleData[1]))
+        assertEquals(15, Day1.getValues(sampleData[2]))
+        assertEquals(77, Day1.getValues(sampleData[3]))
+
+        assertEquals(142, Day1.sumValues(sampleData))
+    }
+
+    @Test
+    fun samplePart2GetValues() {
         val day1 = Day1()
-        assertEquals(3, day1.addTwoNumbers(1, 2))
+
+        val sampleData = listOf("two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2","zoneight234", "7pqrstsixteen")
+
+        assertEquals(29, Day1.getValues(sampleData[0]))
+        assertEquals(83, Day1.getValues(sampleData[1]))
+        assertEquals(13, Day1.getValues(sampleData[2]))
+        assertEquals(24, Day1.getValues(sampleData[3]))
+        assertEquals(42, Day1.getValues(sampleData[4]))
+        assertEquals(14, Day1.getValues(sampleData[5]))
+        assertEquals(76, Day1.getValues(sampleData[6]))
+
+        assertEquals(281, Day1.sumValues(sampleData))
+    }
+
+    fun part2() {
+        val data = Util().readData("day1.txt")
+        assertEquals(56042,Day1.sumValues(data))
     }
 }
