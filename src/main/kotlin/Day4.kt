d4c95acaf62e9d03c0fd4df55e7c9d453506a5a3
martwebs <mwebster1973@gmail.com>
2023-12-04 09:03:04 +0000

    Day 4 - Refactor

--- src/main/kotlin/Day4.kt
+++ src/main/kotlin/Day4.kt
@@ -1,59 +1,46 @@
-class Day4(private val lines: List<String>) {
+import kotlin.math.pow
 
-    data class Card(val winningNumbers: List<Int>, val numbers: List<Int>, val matches: Int){
+class Day4(lines: List<String>) {
+    private val cards = lines.map { Card.fromLine(it) }
+
+    data class Card(val matches: Int, var count: Int = 1){
+        fun incrementCount(valueToIncrement: Int){
+            count += valueToIncrement
+        }
+
+        fun getPoints(): Int {
+            return 2.0.pow(matches.toDouble() - 1).toInt()
+        }
 
         companion object{
             fun fromLine(line: String): Card {
-                val winningNumbers = line.substringAfter(": ").substringBefore(" |").split(" ")
-                    .filter { it.length>0 }
+                val winningNumbers = line
+                    .substringAfter(":")
+                    .substringBefore("|")
+                    .split(" ")
+                    .filter { it.isNotEmpty() }
                     .map { it.toInt() }
-                val cardNumbers = line.substringAfter("| ").split(" ")
-                    .filter { it.length>0 }
+                val cardNumbers = line
+                    .substringAfter("|")
+                    .split(" ")
+                    .filter { it.isNotEmpty() }
                     .map { it.toInt() }
-                val score = winningNumbers.intersect(cardNumbers)
-                return Card(winningNumbers, cardNumbers, score.size);
+                return Card(winningNumbers.intersect(cardNumbers).size)
             }
         }
     }
 
-    companion object{
-
-        // part 1
-        fun getPoints(card: String): Int {
-            val card = Card.fromLine(card);
-
-            if (card.matches==0){
-                return 0;
-            }
-            var total = 1;
-            for (fred in  2..card.matches){
-                total= total*2
-            }
-            return total;
-        }
-        fun sumScore(cards: List<String>): Int{
-            return cards.sumOf { getPoints(it) }
-        }
-
-        fun part2(data: List<String>): Int {
-            val cards = data.map { Card.fromLine(it) }
-            // totals contain 1 of each card
-            val totals = cards.map { 1 }.toIntArray()
-
-            cards.forEachIndexed { index, card ->
-                val score = card.matches;
-                if (score>0) {
-                    for (nextIndex in index+1..index+score ){
-                        val numberOfThisCard = totals[index]
-                        val nextScore = totals.get(nextIndex)+numberOfThisCard
-                        totals.set(nextIndex, nextScore)
-                    }
-                }
-            }
-            return totals.sum()
-        }
-
+    // part 1
+    fun sumScore(): Int{
+        return cards.sumOf { it.getPoints() }
     }
 
-
+    fun part2(): Int {
+        cards.forEachIndexed { index, card ->
+            for (x in 1..card.matches ){
+                cards[index+x].incrementCount(card.count)
+            }
+        }
+        return cards.sumOf { it.count }
+    }
 }
--- src/test/kotlin/Day4Test.kt
+++ src/test/kotlin/Day4Test.kt
@@ -4,47 +4,38 @@ import org.junit.jupiter.api.Assertions.*
 class Day4Test {
 
     private val sample = listOf(
-    "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
-    "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
-    "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
-    "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
-    "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
-    "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+        "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+        "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+        "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+        "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+        "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+        "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
     )
 
     @Test
     fun samplePart1(){
-        assertEquals(8, Day4.getPoints(sample[0]))
-        assertEquals(2, Day4.getPoints(sample[1]))
-        assertEquals(2, Day4.getPoints(sample[2]))
-        assertEquals(1, Day4.getPoints(sample[3]))
-        assertEquals(0, Day4.getPoints(sample[4]))
-        assertEquals(0, Day4.getPoints(sample[5]))
+        assertEquals(8, Day4.Card.fromLine(sample[0]).getPoints())
+        assertEquals(2, Day4.Card.fromLine(sample[1]).getPoints())
+        assertEquals(2, Day4.Card.fromLine(sample[2]).getPoints())
+        assertEquals(1, Day4.Card.fromLine(sample[3]).getPoints())
+        assertEquals(0, Day4.Card.fromLine(sample[4]).getPoints())
+        assertEquals(0, Day4.Card.fromLine(sample[5]).getPoints())
     }
 
     @Test
-    fun samplePart2(){
-        assertEquals(8, Day4.getPoints(sample[0]))
-        assertEquals(2, Day4.getPoints(sample[1]))
-        assertEquals(2, Day4.getPoints(sample[2]))
-        assertEquals(1, Day4.getPoints(sample[3]))
-        assertEquals(0, Day4.getPoints(sample[4]))
-        assertEquals(0, Day4.getPoints(sample[5]))
-    }
-    @Test
     fun part1() {
         val data = Util().readData("day4.txt")
-        assertEquals(26346,  Day4.sumScore(data))
+        assertEquals(26346,  Day4(data).sumScore())
     }
 
     @Test
     fun part2Sample() {
-        assertEquals(30,  Day4.part2(sample))
+        assertEquals(30,  Day4(sample).part2())
     }
 
     @Test
     fun part2() {
         val data = Util().readData("day4.txt")
-        assertEquals(8467762,  Day4.part2(data))
+        assertEquals(8467762,  Day4(data).part2())
     }
 }
