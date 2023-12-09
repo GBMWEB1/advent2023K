f6b895767e7f1e4756787182c8d016c92f153f47
martwebs <mwebster1973@gmail.com>
2023-12-09 08:08:41 +0000

    Day 9 - Refactor

--- src/main/kotlin/Day9.kt
+++ src/main/kotlin/Day9.kt
@@ -1,79 +1,75 @@
-class Day9(val readings: List<MutableList<Int>>) {
+class Day9(private val readings: List<MutableList<Int>>) {
+
+    companion object {
+        fun ofList(input:  List<String>): Day9 {
+            return Day9(input
+                .map { line -> line.split(" ")
+                    .map { it.toInt() }
+                    .toMutableList() }
+            )
+        }
+        fun of(input:  String): Day9 {
+            return Day9(
+                listOf(
+                    input.split(" ")
+                        .map { it.toInt() }
+                        .toMutableList()
+                )
+            )
+        }
+    }
 
     // part 1
     fun sumHistory(): Int {
         return readings.sumOf { getHistory(it)  }
     }
 
-    // part 2
-    fun sumLeftMostHistory(): Int {
-        return readings.sumOf { getLeftHistory(it)  }
-    }
-
     private fun getHistory(inputRow: MutableList<Int>): Int {
-        var row = inputRow
-        var readingRows = mutableListOf(row)
-        while (row.any { it !=0 }){
-            row = createRow(row);
-            readingRows.add(row)
-        }
-        return calculateNextValue(readingRows)
+        return calculateNextValue(expandToList(inputRow))
     }
 
-    fun getLeftHistory(inputRow: MutableList<Int>): Int {
-        var row = inputRow
-        var readingRows = mutableListOf(row)
-        while (row.any { it !=0 }){
-            row = createRow(row);
-            readingRows.add(row)
+    private fun expandToList(inputRow: MutableList<Int>): MutableList<MutableList<Int>> {
+        val rows = mutableListOf(inputRow)
+        while (rows.last().any { it != 0 }) {
+            rows.add(createRow(rows.last()))
         }
-        return calculateFirstValue(readingRows)
+        return rows
+    }
+
+    private fun calculateNextValue(rows: MutableList<MutableList<Int>>): Int{
+        val readingRows = rows.reversed()
+        readingRows[0].add(0)
+        for (x in 0..readingRows.size-2){
+            val newValue = readingRows[x].last() + readingRows[x+1].last()
+            readingRows[x+1].add(newValue)
+        }
+        return readingRows.last().last()
+    }
+    fun createRow(row: MutableList<Int>): MutableList<Int> {
+        val newRow = mutableListOf<Int>()
+        for (pos in 0..row.size-2){
+            newRow.add(row[pos+1] - row[pos])
+        }
+        return newRow
+    }
+
+    // part 2
+    fun sumLeftHistory(): Int {
+        return readings.sumOf { getFirstValue(it)  }
+    }
+
+    private fun getFirstValue(inputRow: MutableList<Int>): Int {
+        return calculateFirstValue(expandToList(inputRow))
     }
 
     private fun calculateFirstValue(rows: MutableList<MutableList<Int>>): Int {
         val readingRows = rows.reversed()
         readingRows[0].add(0)
         for (x in 0..readingRows.size-2){
-
-            var newValue = readingRows[x+1].first() - readingRows[x].first()
+            val newValue = readingRows[x+1].first() - readingRows[x].first()
             readingRows[x+1].add(0, newValue)
         }
         return readingRows.last().first()
-    }
-
-    fun calculateNextValue(rows: MutableList<MutableList<Int>>): Int{
-        val readingRows = rows.reversed()
-        readingRows[0].add(0)
-        for (x in 0..readingRows.size-2){
-            var newValue = readingRows[x].last() + readingRows[x+1].last()
-            readingRows[x+1].add(newValue)
-        }
-        return readingRows.last().last()
-    }
-
-
-    fun getFirstHistory(): Int {
-        var row = readings[0]
-        return getHistory(row)
-    }
-
-    fun createRow(row: MutableList<Int>): MutableList<Int> {
-        var newRow = mutableListOf<Int>()
-        for (pos in 0..row.size-2){
-            newRow.add(row[pos+1] - row[pos])
-        }
-        return newRow;
-    }
-
-
-
-    companion object {
-        fun ofList(input:  List<String>): Day9 {
-            return Day9(input.map { it.split(" ").map { it.toInt() }.toMutableList() })
-        }
-        fun of(input:  String): Day9 {
-            return Day9( listOf(input.split(" ").map { it.toInt() }.toMutableList() ))
-        }
     }
 
 }
--- src/test/kotlin/Day9Test.kt
+++ src/test/kotlin/Day9Test.kt
@@ -6,50 +6,47 @@ class Day9Test {
     fun sample1(){
     val map = Day9.of("0 3 6 9 12 15")
         assertEquals(listOf(3,3,3,3,3), map.createRow(mutableListOf(0,3,6,9,12,15)))
-        assertEquals(18, map.getFirstHistory());
+        assertEquals(18, map.sumHistory())
     }
 
     @Test
     fun sample2(){
         val map = Day9.of("1 3 6 10 15 21")
-        assertEquals(28, map.getFirstHistory());
+        assertEquals(28, map.sumHistory())
     }
 
     @Test
     fun sample3(){
         val map = Day9.of("10 13 16 21 30 45")
-        assertEquals(68, map.getFirstHistory());
+        assertEquals(68, map.sumHistory())
     }
 
-    // test 8 6 4 2 0 -2 -4 -6 (sequences going down)
     @Test
     fun sample4(){
         val map = Day9.of("8 6 4 2 0 -2 -4 -6")
-        assertEquals(-8, map.getFirstHistory());
+        assertEquals(-8, map.sumHistory())
     }
 
     @Test
     fun sample5(){
         val map = Day9.of("-2 -4 -6")
-        assertEquals(-8, map.getFirstHistory());
+        assertEquals(-8, map.sumHistory())
     }
 
     @Test
     fun part1() {
         val data = Util().readData("day9.txt")
-        val map = Day9.ofList(data);
-        assertEquals(1995001648, map.sumHistory());
+        assertEquals(1995001648, Day9.ofList(data).sumHistory())
     }
     @Test
     fun samplePart2(){
-        assertEquals(-3, Day9.of("0 3 6 9 12 15").sumLeftMostHistory());
-        assertEquals(0, Day9.of("1 3 6 10 15 21").sumLeftMostHistory());
-        assertEquals(5, Day9.of("10 13 16 21 30 45").sumLeftMostHistory());
+        assertEquals(-3, Day9.of("0 3 6 9 12 15").sumLeftHistory());
+        assertEquals(0, Day9.of("1 3 6 10 15 21").sumLeftHistory());
+        assertEquals(5, Day9.of("10 13 16 21 30 45").sumLeftHistory());
     }
     @Test
     fun part2() {
         val data = Util().readData("day9.txt")
-        val map = Day9.ofList(data);
-        assertEquals(988, map.sumLeftMostHistory());
+        assertEquals(988, Day9.ofList(data).sumLeftHistory());
     }
 }
